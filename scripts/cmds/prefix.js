f14d4faf8dbf2819c56b06b9dc5c1dee54c7b181
ᏔᎯᏁᎠᎬᏒᎬᏒ <mogarishishikuno99@gmail.com>
2026-09-22 04:47:10 +0100

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -1,9 +1,9 @@
-const fonts = require("../func/fonts.js");
+const fonts = require("../func/fonts.js");const axios = require("axios");
 
 module.exports = {
   config: {
     name: "prefix",
-    version: "1.2.0",
+    version: "1.3.0",
     author: "Shade × Gemini",
     countDown: 3,
     role: 0,
@@ -21,15 +21,34 @@ module.exports = {
     } catch (e) {}
     
     const message = 
-      `𝗦alut 🍃${userName} tu veux savoir ma vision ?\n` +
+      `𝗘chanté ${userName} tu veux savoir ma vision ?\n` +
       `╭‣ 🌐 𝗚𝗅𝗈𝖻𝖺𝗅: ${currentPrefix}\n` +
       `╰‣ 💬 𝗖e 𝗖𝗁𝖺𝗍: ${currentPrefix}\n` +
-      `🍃𝗝e suis ᏔᎯᏁᎠᎬᏒᎬᏒ\n` +
+      `🍃 I'm ᎠᎯᎬᎷᎾᏁ\n` +
       `📂 𝗘ssayer "${currentPrefix}𝗁𝖾𝗅𝗉" pour voir toutes les commandes.`;
+
+    // URLs des images à joindre
+    const imgUrls = [
+      "https://i.imgur.com/zecPsPU.jpeg",
+      "https://i.imgur.com/QOjUcq8.jpeg"
+    ];
+
+    const attachments = [];
+    
+    // Téléchargement des images sous forme de flux (stream)
+    for (const url of imgUrls) {
+      try {
+        const response = await axios.get(url, { responseType: "stream" });
+        attachments.push(response.data);
+      } catch (err) {
+        console.error("Erreur lors du chargement de l'image :", err);
+      }
+    }
     
     return api.sendMessage(
       {
         body: message,
+        attachment: attachments,
         mentions: [{
           tag: userName,
           id: senderID
@@ -39,5 +58,4 @@ module.exports = {
       messageID
     );
   }
-}
-  ;
+};
